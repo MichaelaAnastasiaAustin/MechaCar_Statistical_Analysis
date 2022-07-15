87f491e81893b4e38bbe0864944e323e8e76af6a
Michaela Austin <michaelaaustin27@gmail.com>
2022-07-14 22:59:21 -0700

    updated script

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -5,5 +5,10 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+t.test(suspension_coil$PSI,mu=1500)
 
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
                                               
+       
