535b0bd6b4fbeaf0ff81032a5e106ad9fc8d4d22
Michaela Austin <michaelaaustin27@gmail.com>
2022-07-14 21:58:14 -0700

    updated R Script

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,3 +2,8 @@ mecha_car <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car)
 )
+suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+                                              
